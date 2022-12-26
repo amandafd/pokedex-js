9af4e874b08e6f19334611dbc58df019114d7e66
amandafd <amanda_dionisio@hotmail.com>
2022-12-25 22:24:54 -0300

    feat: adding renderPokemon function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
+const pokemonName = document.querySelector('.pokemon__name')
+
 const fetchPokemon = async (pokemon) => {
-
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
+    const data = await APIResponse.json();
+    return data;
 }
 
-fetchPokemon();
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+  
+    pokemonName.innerHTML = data.name;
+}
+
+renderPokemon('25');
