166357c3382b5b2c3f753b5e36c6b3466478a844
amandafd <amanda_dionisio@hotmail.com>
2022-12-26 11:08:21 -0300

    feat: adding pokemon animated gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-const pokemonName = document.querySelector('.pokemon__name')
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -10,6 +12,8 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
   
     pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
 renderPokemon('25');
